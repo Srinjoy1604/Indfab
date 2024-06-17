9bd9eca22753c2e5a12a2d8fb2069705c71cf6ea
Srinjoy1604 <srinjoy12345@gmail.com>
2024-06-17 13:50:29 +0530

    Initial commit2

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-modal-image'],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
