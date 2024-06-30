c62d0c2cebdc9b18c1523a4955d86a379bdfb31e
Srinjoy1604 <srinjoy12345@gmail.com>
2024-06-30 13:52:41 +0530

    Changes in Machine Card

--- src/components/Machine_cards/Machine_card.jsx
+++ src/components/Machine_cards/Machine_card.jsx
@@ -10,37 +10,39 @@ function MachineCard({ image, name, specs }) {
 
     return (
         <div className={`${styles.Carddiv}`}>
-            <div className={`${styles.Imagediv}`}><img src={image}></img></div>
-            <div className={`${styles.Namediv}`}><h2>{name}</h2></div>
-            
             <div className={`${styles.Accordiondiv}`}>
-                <Accordion type="single" collapsible className={`${styles.AccordionBar}`}>
+                <Accordion type="single" collapsible className={`${styles.AccordionBar}`} orientation="horizontal">
                     <AccordionItem value="item-1" className={`${styles.AccordionItem}`}>
                         <AccordionTrigger className={`${styles.AccordionTrigger}`}>Specifications</AccordionTrigger>
-                        <AccordionContent className={`${styles.AccordionContent}`}>
-                        <table>
-                        <thead>
-                            <tr>
-                                <th>Description</th>
-                                <th>Tech. Specifications</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                specs.map((spec, index) => (
-                                    <tr key={index}>
-                                        <td>{spec.title}</td>
-                                        <td>{spec.desc}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                
-                        </AccordionContent>
+                        <div className={`${styles.AccordionContentDiv}`}>
+                            <AccordionContent className={`${styles.AccordionContent}`}>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Description</th>
+                                            <th>Tech. Specifications</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            specs.map((spec, index) => (
+                                                <tr key={index}>
+                                                    <td>{spec.title}</td>
+                                                    <td>{spec.desc}</td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
+                            </AccordionContent>
+                        </div>
                     </AccordionItem>
                 </Accordion>
             </div>
+            <div className={`${styles.Imagediv}`}><img src={image}></img></div>
+            <div className={`${styles.Namediv}`}><h2>{name}</h2></div>
+
+
 
 
         </div>
